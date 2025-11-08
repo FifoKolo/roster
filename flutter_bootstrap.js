13064124a432a72f38a3094c5c54ad1eb81cdb0d
Filip Kolarsky <148553297+FifoKolo@users.noreply.github.com>
2025-11-08 21:40:37 +0000

    path issues fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1672014948"
+    serviceWorkerVersion: "2487630423"
   }
 });
